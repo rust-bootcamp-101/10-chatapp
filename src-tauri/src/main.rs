df3c2c1cf019582d74c72a8a0289ef35bf8e7443
startdusk <522383345@qq.com>
2024-09-10 22:21:09 +0800

    chore: add github actions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,6 +7,6 @@ use chatapp_lib::app;
 fn main() -> Result<()> {
     app()?
         .run(tauri::generate_context!())
-        .expect("error while runing tauri application");
+        .expect("error while running tauri application");
     Ok(())
 }
